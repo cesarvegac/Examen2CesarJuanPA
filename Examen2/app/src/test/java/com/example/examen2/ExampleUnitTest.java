cb72c8af0b6174d09b50fc03aeb390b3d869fcf1
cesar <cesarvega1854@gmial.com>
2024-11-10 13:13:09 -0600

    Se agreg mi prueba Unitaria

--- Examen2/app/src/test/java/com/example/examen2/ExampleUnitTest.java
+++ Examen2/app/src/test/java/com/example/examen2/ExampleUnitTest.java
@@ -1,17 +1,54 @@
-package com.example.examen2;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import com.example.examen2.Calculadora;
 
-import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testConvertirNumero_Binario() {
+        assertEquals(5, Calculadora.convertirNumero("101", 0)); // 101 binario = 5 decimal
+        assertEquals(10, Calculadora.convertirNumero("1010", 0)); // 1010 binario = 10 decimal
+    }
+
+    @Test
+    public void testConvertirNumero_Octal() {
+        assertEquals(8, Calculadora.convertirNumero("10", 1)); // 10 octal = 8 decimal
+        assertEquals(64, Calculadora.convertirNumero("100", 1)); // 100 octal = 64 decimal
+    }
+
+    @Test
+    public void testConvertirNumero_Decimal() {
+        assertEquals(123, Calculadora.convertirNumero("123", 2)); // 123 decimal = 123 decimal
+        assertEquals(0, Calculadora.convertirNumero("0", 2)); // 0 decimal = 0 decimal
+    }
+
+    @Test
+    public void testConvertirNumero_Hexadecimal() {
+        assertEquals(255, Calculadora.convertirNumero("FF", 3)); // FF hexadecimal = 255 decimal
+        assertEquals(4096, Calculadora.convertirNumero("1000", 3)); // 1000 hexadecimal = 4096 decimal
+    }
+
+    @Test
+    public void testConvertirNumero_BaseNoSoportada() {
+        assertEquals(-1, Calculadora.convertirNumero("101", 4)); // Base no soportada
+    }
+
+    @Test
+    public void testConvertirNumero_InputInvalido() {
+        assertEquals(-1, Calculadora.convertirNumero("2AB", 0)); // Input no válido en binario
+        assertEquals(-1, Calculadora.convertirNumero("89", 1)); // Input no válido en octal
+        assertEquals(-1, Calculadora.convertirNumero("12.3", 2)); // Input no válido en decimal
+        assertEquals(-1, Calculadora.convertirNumero("GHI", 3)); // Input no válido en hexadecimal
+    }
+
+    @Test
+    public void testConvertirNumero_InputVacio() {
+        assertEquals(-1, Calculadora.convertirNumero("", 2)); // Input vacío en decimal
+    }
+
+    @Test
+    public void testConvertirNumero_InputNull() {
+        assertEquals(-1, Calculadora.convertirNumero(null, 2)); // Input nulo en decimal
     }
 }
